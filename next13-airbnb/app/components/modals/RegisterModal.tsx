e9deb98d97d80d7dda173d5e92d341c96a9a3cb8
rmnsrs <ramanshrestha@eeposit.com>
2023-05-08 21:57:47 +0545

    Register Modal UI complete

--- next13-airbnb/app/components/modals/RegisterModal.tsx
+++ next13-airbnb/app/components/modals/RegisterModal.tsx
@@ -7,6 +7,10 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal = () => {
 	const registerModal = useRegisterModal();
@@ -29,11 +33,87 @@ const RegisterModal = () => {
 		axios
 			.post("/api/register", data)
 			.then(() => registerModal.onClose())
-			.catch((error) => console.log(error))
+			.catch((error) => toast.error("Server error!"))
 			.finally(() => setIsLoading(false));
 	};
 
-	const bodyContent = <div className="flex flex-col gap-4"></div>;
+	const bodyContent = (
+		<div className="flex flex-col gap-4">
+			<Heading title="Welcome to AirBnb" subtitle="Create An Account!" />
+			<Input
+				id="email"
+				label="Email"
+				disabled={isLoading}
+				register={register}
+				errors={errors}
+				required
+			/>
+
+			<Input
+				id="name"
+				label="Name"
+				disabled={isLoading}
+				register={register}
+				errors={errors}
+				required
+			/>
+
+			<Input
+				id="password"
+				type="password"
+				label="Password"
+				disabled={isLoading}
+				register={register}
+				errors={errors}
+				required
+			/>
+		</div>
+	);
+
+	const footerContent = (
+		<div className="flex flex-col gap-4 mt-3">
+			<hr />
+			<Button
+				outline
+				label="Continue with Google"
+				icon={FcGoogle}
+				onClick={() => {}}
+			/>
+			<Button
+				outline
+				label="Continue with Github"
+				icon={AiFillGithub}
+				onClick={() => {}}
+			/>
+			<div
+				className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+			>
+				<div
+					className="
+				flex
+				flex-row
+				justify-center
+				items-center gap-2"
+				>
+					<div>Already have an account? </div>
+					<div
+						onClick={registerModal.onClose}
+						className="
+				text-neutral-800
+				cursor-pointer
+				hover:underline"
+					>
+						Log In
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 
 	return (
 		<Modal
@@ -44,6 +124,7 @@ const RegisterModal = () => {
 			onClose={registerModal.onClose}
 			onSubmit={handleSubmit(onSubmit)}
 			body={bodyContent}
+			footer={footerContent}
 		/>
 	);
 };
